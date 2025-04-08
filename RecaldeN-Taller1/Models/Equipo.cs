7e32bc22f89123bb3cd6b698b03a96574bedbe8c
Nicolasre18 <nicolasre18@gmail.com>
2025-04-08 12:40:12 -0500

    Add project files.

---
+++ RecaldeN-Taller1/Models/Equipo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecaldeN_Taller1.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Nombre del equipo")]
+        public string Nombre { get; set; }
+        [Range(0, 100)]
+        public int PartidosJugados { get; set; }
+        [Range(0, 100)]
+        public int PartidosGanados { get; set; }
+        [Range(0, 100)]
+        public int PartidosPerdidos { get; set; }
+        [Range(0, 100)]
+        public int PartidosEmpatados { get; set; }
+    }
+}
---
+++ RecaldeN-Taller1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RecaldeN_Taller1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
